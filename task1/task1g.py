c8374e6a2e4d451df5462a566f58f35a1ef11774
Thorbjørn Djuvpik <thorbjorn.djupvik@gmail.com>
2024-11-02 18:09:54 +0100

    started on priority-queue

--- task1/task1g.py
+++ task1/task1g.py
@@ -12,123 +12,214 @@ T = 50 * 24 * 60
 
 
 class Queue:
-    def __init__(self, max_length: int) -> None:
-        # Array with treatment_times
-        self.p_queue = np.zeros(max_length, dtype=np.int64)
-        self.p_front = 0
-        self.p_back = 0
+    def __init__(self, max_length) -> None:
+        self.max_length = max_length
 
         # Array with treatment_times
-        self.queue = np.zeros(max_length, dtype=np.int64)
-        self.front = 0
+        self.queue = np.zeros(max_length)
+        self.head = 0
         self.back = 0
 
+    def pop(self) -> float:
+        treatment_time = self.queue[self.head]
+        self.head += 1
+        return treatment_time
+
+    def update_treatment_time(self, treatment_time: float):
+        self.queue[self.head] = treatment_time
+
+    def append(self, treatment_time: float):
+        self.queue[self.back] = treatment_time
+        self.back += 1
+
+    @property
+    def treatment_time(self):
+        return self.queue[self.head]
+
+    @property
+    def empty(self):
+        return self.head == self.back
+
+    @property
+    def length(self):
+        if self.head <= self.back:
+            return self.back - self.head
+        return self.back - self.head + self.max_length
+
+
+class UCC:
+    def __init__(self, m: float, l: float, p: float, T: float) -> None:
+        self.m = m
+        self.l = l
+        self.p = p
+        self.T = T
+        self.current_time = 0
+
+        # Init random variables
+        self.n_patients = np.random.poisson(self.l * T)
+        self.classification = np.random.binomial(1, self.p, size=self.n_patients)
+        print(self.classification)
+        self.n_urgent_patients = np.count_nonzero(self.classification)
+        print(self.n_urgent_patients)
+        self.n_normal_patients = self.n_patients - self.n_urgent_patients
+
+        # We know how many customers arrived in the 50 days, so the arrival times
+        # are uniformly distributed
+        self.arrival_times = np.sort(np.random.uniform(0, T, size=self.n_patients))
+
+        # The treatment times are exponentially distributed
+        self.treatment_times = np.random.exponential(1 / mu, size=self.n_patients)
+
+        # Array with treatment_times
+        self.priority_queue = Queue(self.n_urgent_patients)
+        self.normal_queue = Queue(self.n_normal_patients)
+
+        # Index into how far we have many patients have arrived
+        self.i_patients = 0
+
         self.service = 0  # 0: None, 1: priority, 2: normal
-        self.service_end = 0
+        self.service_endtime = np.inf
 
-    def append(self, arrival_time: float, treatment_time: float, priority: bool):
-        if priority:
-            self.p_queue[self.p_back] = treatment_time
-            self.p_back += 1
-            if self.p_back == len(self.p_queue):
-                self.p_back = 0
+    def next_step(self) -> bool:
+        if (
+            self.i_patients < self.n_patients - 1
+            and self.arrival_times[self.i_patients + 1] <= self.service_endtime
+        ):
+            self.current_time = self.arrival_times[self.i_patients + 1]
+            self.new_arrival()
         else:
-            self.queue[self.back] = treatment_time
-            self.back += 1
-            if self.back == len(self.queue):
-                self.back = 0
+            self.current_time = self.service_endtime
+            # If the current_time exceed T, we are done with the simulation
+            if self.current_time > self.T:
+                return True
+            self.complete_service()
+        return False
 
-        # Start treatment
-        if self.service == 0:
-            self.start_service(arrival_time, treatment_time, priority)
-        elif self.service == 2 and priority:
-            self.start_service(arrival_time, treatment_time, priority)
-
-    def start_service(self, current_time, treatment_time, priority):
+    def new_arrival(self):
+        self.i_patients += 1
+        priority = self.classification[self.i_patients]
+        treatment_time = self.treatment_times[self.i_patients]
         if priority:
+            self.priority_queue.append(treatment_time)
+        else:
+            self.normal_queue.append(treatment_time)
+
+        if self.service == 0:
+            self.start_service()
+        elif self.service == 2 and priority:
+            self.interrupt_service()
+
+    def interrupt_service(self):
+        # Create new treatment time to the interrupted patient
+        self.normal_queue.update_treatment_time(np.random.exponential(1 / self.m))
+        self.start_service()
+
+    def start_service(self):
+        assert not (
+            self.priority_queue.empty and self.normal_queue.empty
+        ), "We can not start a service if the queue is empty."
+
+        if not self.priority_queue.empty:
             self.service = 1
+            treatment_time = self.priority_queue.treatment_time
         else:
             self.service = 2
-        self.service_end = current_time + treatment_time
+            treatment_time = self.normal_queue.treatment_time
+        self.service_endtime = self.current_time + treatment_time
 
-    def end_service(self):
-        pass
+    def complete_service(self):
+        assert not (
+            self.priority_queue.empty and self.normal_queue.empty
+        ), "Can not complete a service if none are being served."
+        # One patient walk out
+        if self.service == 1:
+            self.priority_queue.pop()
+        else:
+            self.normal_queue.pop()
 
-    def pop(self, priority: bool):
-        pass
+        # One new patient starts recieving treatment if there is a patient in the queue
+        if not (self.priority_queue.empty and self.normal_queue.empty):
+            self.start_service()
+        else:
+            # If there are no patients, none are treated
+            self.service = 0
+            self.service_endtime = np.inf
+
+    @property
+    def N(self):
+        return self.normal_queue.length
+
+    @property
+    def U(self):
+        return self.priority_queue.length
+
+    @property
+    def X(self):
+        return self.N + self.U
 
 
-def simulate_realization():
-    # Get how many customers arrived in 50 days
-    n_patients = np.random.poisson(lam * T)
-    n_urgent_patients = np.random.binomial(n_patients, p)
-    n_normal_patients = n_patients - n_urgent_patients
-    # We know how many customers arrived in the 50 days, so the arrival times
-    # are uniformly distributed.
-    urgent_arrival_times = np.sort(np.random.uniform(0, T, size=n_urgent_patients))
-    normal_arrival_times = np.sort(np.random.uniform(0, T, size=n_normal_patients))
-    # We need at maximum n_patients**2 treating times
-    urgent_treatment_times = np.random.exponential(1 / mu, size=n_urgent_patients)
-    normal_treatment_times = np.random.exponential(1 / mu, size=n_patients)
+def simulate_realization(m, l, p, T) -> tuple[
+    tuple[np.ndarray, np.ndarray],
+    tuple[np.ndarray, np.ndarray],
+    tuple[np.ndarray, np.ndarray],
+]:
+    ucc = UCC(m, l, p, T)
 
     # At most 2*n_customers events will happen
-    N = np.zeros(2 * n_normal_patients)
-    U = np.zeros(2 * n_urgent_patients)
-    X = np.zeros(2 * n_patients)
-    time_U = np.zeros(2 * n_urgent_patients)
-    time_N = np.zeros(2 * n_normal_patients)
-    time_X = np.zeros(2 * n_patients)
+    N = np.zeros(2 * ucc.n_normal_patients + 1)
+    U = np.zeros(2 * ucc.n_urgent_patients + 1)
+    X = np.zeros(2 * ucc.n_patients + 1)
+    time_N = np.zeros_like(N)
+    time_U = np.zeros_like(U)
+    time_X = np.zeros_like(X)
 
-    u_arrival = 1  # Arrival index
-    u_treatment = 0  # Releasetime index
+    iN, iU, iX = 1, 1, 1
 
-    i_arrival = 1  # Arrival index
-    i_treatment = 0  # Releasetime index
-
-    # Init
-    X[0] = 1
-    if urgent_arrival_times[0] <= normal_arrival_times[0]:
-        time[0] = urgent_arrival_times[0]
-        U[0] = 1
-    else:
-        pass
-
-    while i_a < n_patients and time[i_a + i_r] <= T:
-        # If someone arrives before current service is done
-        if arrival_times[i_a] < release_time:
-            X[i_a + i_r] = X[i_a + i_r - 1] + 1
-            time[i_a + i_r] = arrival_times[i_a]
-            i_a += 1
-        # Else the next event is that the service is done
-        else:
-            X[i_a + i_r] = X[i_a + i_r - 1] - 1
-            time[i_a + i_r] = release_time
-            i_r += 1
-            if X[i_a + i_r - 1] == 0:
-                # If X == 0, the next release time is the arrival_time of the next customer + its service time
-                release_time = arrival_times[i_r] + treatment_times[i_r]
-            else:
-                # Else the next release time is the current time + next service time
-                release_time = release_time + treatment_times[i_r]
+    done = False
+    while not done:
+        done = ucc.next_step()
+        if done:
+            break
+        n, u, x = ucc.N, ucc.U, ucc.X
+        current_time = ucc.current_time
+        if N[iN - 1] != n:
+            N[iN] = n
+            time_N[iN] = current_time
+            iN += 1
+        if U[iU - 1] != u:
+            U[iU] = u
+            time_U[iU] = current_time
+            iU += 1
+        if X[iX - 1] != x:
+            X[iX] = x
+            time_X[iX] = current_time
+            iX += 1
 
     # Only return the relevant parts of the array, the rest is still zero
-    return time[: i_a + i_r], X[: i_a + i_r]
+    return (
+        (time_N[: iN + 1], N[: iN + 1]),
+        (time_U[: iU + 1], U[: iU + 1]),
+        (time_X[: iX + 1], X[: iX + 1]),
+    )
 
 
 def average_time_spent():
-    t, x = simulate_realization()
+    n, u, x = simulate_realization(mu, lam, p, T)
+    print(n)
+    print(u)
+    print(x)
     # Construct weights for the weighted average
-    delta_t = np.zeros_like(t)
-    delta_t[:-1] = t[1:] - t[:-1]
-    delta_t[-1] = T - t[-1]
+    # delta_t = np.zeros_like(t)
+    # delta_t[:-1] = t[1:] - t[:-1]
+    # delta_t[-1] = T - t[-1]
 
     # L is the average of X weighted by the time X stayed constant
-    L = np.average(x, 0, delta_t)
+    # L = np.average(x, 0, delta_t)
     # We start a bit out in the array so the state has "stabilized"
     # L = np.average(x[100:], 0, delta_t[100:])
 
     # By Little's law
-    return L / lam
+    # return L / lam
 
 
 def CI():
@@ -154,23 +245,23 @@ def plot_steps(axs, x: np.ndarray, y: np.ndarray, end: float, color, **kwargs):
     return line
 
 
-def task1b():
-    t, x = simulate_realization()
-    t /= 60  # Convert to hours
+def task1g():
     stop_time = 12  # minutes in 12 hours
-    (indices,) = np.where(t <= stop_time)
+    n, u, x = simulate_realization(mu, lam, p, 12 * 60)
+    # (indices,) = np.where(t <= stop_time)
 
     fig, axs = plt.subplots()
-    line1 = plot_steps(axs, t[indices], x[indices], stop_time, "blue")
+    line1 = plot_steps(axs, x[0] / 60, x[1], stop_time, "blue")
     axs.legend([line1], ["X"])
     axs.grid()
     fig.tight_layout()
     fig.savefig("./plots/task1b.png")
 
-    avg, lower, upper = CI()
-    print()
-    print("Avg: ", avg)
-    print(f"CI: [{lower:.3f}, {upper:.3f}]")
+    # avg, lower, upper = CI()
+    # print()
+    # print("Avg: ", avg)
+    # print(f"CI: [{lower:.3f}, {upper:.3f}]")
 
 
-task1b()
+# average_time_spent()
+task1g()
